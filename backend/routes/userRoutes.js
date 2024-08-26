5341c808fe8c930f4ea234224d99411be64e0519
Robin Daems <robindaems1@gmail.com>
2024-08-26 05:57:20 +0200

    feat: delete user account- admin only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
 import getAllUserAccounts from "../controllers/user/getAllUserAccounts.js";
+import deleteUserAccount from "../controllers/user/deleteUserAccount.js";
 import role from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
@@ -17,5 +18,9 @@ router
 router
   .route("/all")
   .get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUserAccounts);
+
+router
+  .route("/id")
+  .delete(checkAuth, role.checkRole(role.ROLES.Admin), deleteUserAccount);
 
 export default router;
